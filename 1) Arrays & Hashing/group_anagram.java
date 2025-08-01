0c266cc6522f0da92f173124732d1bd4ba999662
mzaidrizwan <z0887451@gmail.com>
2025-08-01 22:56:41 +0500

    organizing anagrams sets in different arrays.All data is coming in single array.

---
+++ 1) Arrays & Hashing/group_anagram.java
@@ -0,0 +1,71 @@
+public class group_anagram {
+    public static void main(String[] args) {
+        String[] group = { "act", "cat"};
+        String[] result = anagram_identifier(group);
+    }
+
+    static String[] anagram_identifier(String[] group) {
+
+        for (int i = 0; i < group.length; i++) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            char[] toFocus = group[i].toCharArray();
+
+            for (int j = 0; j < group.length; j++) {
+
+
+
+                
+
+                if (i == j)
+                    continue;
+
+                char[] toCheck = group[j].toCharArray();
+
+                if (toFocus.length == toCheck.length) {
+                    boolean found = false;
+
+                    for (int k = 0; k < toCheck.length; k++) {
+                        for (int k2 = 0; k2 < toCheck.length; k2++) {
+                            // int count=0;
+
+                            if (toFocus[i] == toCheck[j] && toFocus[i] != '-') {
+                                found = true;
+                                toFocus[i] = '-';
+                                toCheck[j] = '-';
+                            }
+
+                            if (!found) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return group;
+    }
+}
